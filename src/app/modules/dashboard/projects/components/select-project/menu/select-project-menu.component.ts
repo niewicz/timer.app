d8cec0c30fbd7c19fcb8ad9bc4e715dd965e8452
Tomasz Próchniewicz <tommpoland@gmail.com>
2017-12-17 23:21:34 +0100

    select project menu

--- src/app/modules/dashboard/projects/components/select-project/menu/select-project-menu.component.ts
+++ src/app/modules/dashboard/projects/components/select-project/menu/select-project-menu.component.ts
@@ -1,14 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+
 import { IProject } from '../../../../shared/projects/projects.interfaces';
+
 @Component({
   selector: 'timer-select-project-menu',
   templateUrl: './select-project-menu.component.html',
   styleUrls: ['./select-project-menu.component.scss'],
 })
 export class SelectProjectMenuComponent {
-  @Input() projects: IProject;
+  @Input() projects: IProject[];
+  @Input() selected: IProject;
+  @Input() text: string;
+
+  @Output() choice = new EventEmitter<IProject>();
 
   onInput(event: any) {
     console.log(event);
+  }
+
+  onClick(project: IProject) {
+    this.choice.emit(project);
   }
 }
