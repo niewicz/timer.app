3120b1a68a3f8cbe3cc846c45eb4fe60b82ff624
Tomasz Próchniewicz <tommpoland@gmail.com>
2017-12-25 22:59:25 +0100

    styling <3

--- src/app/modules/dashboard/navigation/containers/main-navbar.component.ts
+++ src/app/modules/dashboard/navigation/containers/main-navbar.component.ts
@@ -29,7 +29,7 @@ import { AuthService } from '../../../auth/shared/auth.service';
           '*',
           stagger('40ms', [
             animate(
-              '0.3s ease-in',
+              '0.25s ease-in',
               keyframes([
                 style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
                 style({ opacity: 1, transform: 'translateY(0)', offset: 1.0 }),
@@ -46,7 +46,7 @@ import { AuthService } from '../../../auth/shared/auth.service';
         query(
           '*',
           stagger('100ms', [
-            animate('0.3s ease-in', keyframes([style({ opacity: 0 })])),
+            animate('0.3s ease-out', keyframes([style({ opacity: 0 })])),
           ]),
           { optional: true },
         ),
