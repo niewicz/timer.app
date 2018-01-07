9f4569e40252ac9da0fd927707bcb5b88df8b9db
Tomasz Próchniewicz <tommpoland@gmail.com>
2018-01-07 22:50:32 +0100

    styling projects list

--- src/app/modules/dashboard/projects/components/projects/item/projects-item.component.ts
+++ src/app/modules/dashboard/projects/components/projects/item/projects-item.component.ts
@@ -4,8 +4,19 @@ import { IProject } from '../../../../shared/projects/projects.interfaces';
 @Component({
   selector: 'timer-projects-item',
   templateUrl: './projects-item.component.html',
+  styleUrls: ['./projects-item.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProjectsItemComponent {
   @Input() project: IProject;
+
+  showIcons = false;
+
+  onMouseover(): void {
+    this.showIcons = true;
+  }
+
+  onMouseleave(): void {
+    this.showIcons = false;
+  }
 }
