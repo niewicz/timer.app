119391fda46ae23a9b753e0b9c150dcfd9a21e9c
Tomasz Próchniewicz <tommpoland@gmail.com>
2018-01-07 19:08:35 +0100

    clients styling

--- src/app/modules/dashboard/clients/components/clients/item/clients-item.component.ts
+++ src/app/modules/dashboard/clients/components/clients/item/clients-item.component.ts
@@ -4,6 +4,7 @@ import { IClient } from '../../../../shared/clients/clients.interfaces';
 @Component({
   selector: 'timer-clients-item',
   templateUrl: './clients-item.component.html',
+  styleUrls: ['./clients-item.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ClientsItemComponent {
