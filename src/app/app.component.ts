b8388be547f6b03cc0721dde6e46298e4f5ddf8c
Tomasz Próchniewicz <tomasz.prochniewicz@prowly.com>
2018-06-20 19:08:12 +0200

    poprawka

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Angular2TokenService } from 'angular2-token';
 
 @Component({
-  selector: 'app-root',
+  selector: 'timer-app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
